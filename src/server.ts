0c8205a3199a19981c0f2d6bfe77ce100beda9e1
tiagoribeironfs1 <tiagoribeironfs1@gmail.com>
2022-04-27 21:43:24 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import "reflect-metadata";
+
+import express from "express";
+import routes from "./routes";
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3334, () => {
+  console.log("Server started on port 3334");
+});
+
+export default app;
